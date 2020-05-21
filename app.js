37be17cad25fe9a092cb23d10511bf22de8247c0
liva ratsimbazafy <liva@DESKTOP-887K512.home>
2020-05-21 23:00:31 +0200

    [app.js]-Feat: add all main functions

--- app.js
+++ app.js
@@ -1,1 +1,47 @@
-console.log('Hello world');
+const timeHandler = () => {
+    let date = new Date();
+    
+    let h = date.getHours();    
+    let min = date.getMinutes();
+    let s = date.getSeconds();
+    
+    let realHr = meridiemHandler(h);
+    
+    h = checkHour(h);
+    console.log(h);
+    h = formatToPair(h);
+    min = formatToPair(min);
+    s = formatToPair(s);
+    
+    document.getElementById('clock').innerHTML =`${h} : ${min} : ${s} ${realHr}`;
+}
+
+const meridiemHandler = time => {
+    let meridiem = 'PM';
+    if ( time <= 12 ){
+        meridiem = 'AM';
+    }
+    return meridiem;
+}
+
+const checkHour = time => {
+    if (time > 12){
+        time -= 12;    
+    }
+    if (time === 0) {
+        time = 12;
+    }
+    return time;
+}
+
+const formatToPair = time => {
+    if ( time < 10 ){
+        time = "0" + time;
+    }
+    return time
+}
+
+timeHandler();
+setInterval(() => {
+    timeHandler();
+}, 1000);
